db4d7b7a9fda360367b55a1e5eddfe42dc3c2ac5
smicks11 <jamespeter2225@gmail.com>
2021-01-22 16:09:12 +0100

    Updates

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var video = document.getElementById('video');
 
-// Get access to the camera!
+// Getting access to the camera
 if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
         //video.src = window.URL.createObjectURL(stream);
